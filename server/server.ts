366ee3fd4c06a6dd6880704837aaa58895dc996d
Roman <email.rom.melnyk@gmail.com>
2018-09-12 00:06:19 +0200

    Implement deeplink reload server response

--- server/server.ts
+++ server/server.ts
@@ -9,9 +9,14 @@ import * as bodyParser from 'body-parser';
 import * as cookieParser from 'cookie-parser';
 
 import httpsOptions from './https-cert/index';
-import { testCasesRouter } from './api/test-cases';
+import { Paths } from '../shared/constants';
 
-import { sequelize } from './db/index';
+import { testCasesRouter } from './api/test-cases';
+// import { testSuitesRouter } from './api/test-suites';
+// import { testPlansRouter } from './api/test-plans';
+// import { regressionCyclesRouter } from './api/regression-cycles';
+
+import { sequelize } from './db';
 
 const app = express();
 
@@ -23,7 +28,32 @@ const staticPath = path.resolve(__dirname, '../client-compiled');
 const staticConfig = express.static(staticPath);
 app.use(staticConfig);
 
-app.use('/api/test-cases', testCasesRouter);
+const availablePaths = Object.values(Paths)
+  .reduce((paths, path) => {
+    const newPaths = typeof path === 'string'
+      ? [path]
+      : Object.values(path);
+    return paths.concat(newPaths);
+  }, [])
+  .map(path => `/${path}`);
+
+app.use(`/api/${Paths.TestCases.ALL}`, testCasesRouter);
+// app.use(`/api/${Paths.TestSuites.ALL}`, testSuitesRouter);
+// app.use(`/api/${Paths.TestPlans.ALL}`, testPlansRouter);
+// app.use(`/api/${Paths.RegressionCycles.ALL}`, regressionCyclesRouter);
+
+app.get(availablePaths, (req, res) => {
+  res.sendFile('index.html', { root: staticPath }, (err) => {
+    if (err) {
+      console.error(err);
+    }
+    res.end();
+  });
+});
+
+app.all(/.*/, (req, res) => {
+  res.redirect(`/${Paths.NOT_FOUND}`);
+});
 
 const logMsg = '[http/s] TestMaster server listening on ';
 
