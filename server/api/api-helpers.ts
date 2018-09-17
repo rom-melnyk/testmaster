1dcab63cc8c64e91cab4944b31a06b650753866f
Roman <email.rom.melnyk@gmail.com>
2018-09-17 20:48:12 +0200

    Introduce all CRUD methods for /api/test-cases

--- server/api/api-helpers.ts
+++ server/api/api-helpers.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 
-export function sendError(res: express.Response, message: string, debug: any) {
+export function sendError(res: express.Response, message: string, debug?: any) {
   res.status(500).send({
     error: true,
     message,
--- server/api/test-cases.ts
+++ server/api/test-cases.ts
@@ -15,12 +15,42 @@ testCasesRouter.get('/', (req, res) => {
   });
 });
 
-testCasesRouter.post('/', (req, res) => {
-  TestCase.upsert(req.body).then((result) => {
-    res.send({ result });
+testCasesRouter.get('/:id', (req, res) => {
+  TestCase.findById(req.params.id).then((testCase) => {
+    if (testCase) {
+      res.send(testCase);
+    } else {
+      sendError(res, 'Not found');
+    }
   }).catch((e) => {
     sendError(res, 'DB error', e);
   });
+});
+
+testCasesRouter.post('/', (req, res) => {
+  TestCase.create(req.body).then((result) => {
+    res.send(result.toJSON());
+  }).catch((e) => {
+    sendError(res, 'DB error', e);
+  });
+});
+
+testCasesRouter.put('/:id', (req, res) => {
+  TestCase.update(req.body, { where: { id: req.params.id } })
+    .then(([ updated ]) => {
+      res.send({ updated });
+    }).catch((e) => {
+      sendError(res, 'DB error', e);
+    });
+});
+
+testCasesRouter.delete('/:id', (req, res) => {
+  TestCase.destroy({ where: { id: req.params.id } })
+    .then((deleted) => {
+      res.send({ deleted });
+    }).catch((e) => {
+      sendError(res, 'DB error', e);
+    });
 });
 
 export { testCasesRouter };
